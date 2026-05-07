2b3cd5a443648f351bd5d97caf42319cc42f342a
hottuspreetus <PIATDV1@student.op.ac.nz>
2026-05-07 16:31:15 +1200

    added cs proj

---
+++ yuh the project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace yuh_the_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
